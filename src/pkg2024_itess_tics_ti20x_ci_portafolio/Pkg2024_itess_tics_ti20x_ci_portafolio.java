302475814db3d0b453cbc3f869f0029688e61d1d
jesus <jesusnicolasgarciamol@gmail.com>
2024-05-24 13:31:49 -0600

    Add class SerrieAdd

---
+++ src/pkg2024_itess_tics_ti20x_ci_portafolio/Pkg2024_itess_tics_ti20x_ci_portafolio.java
@@ -0,0 +1,20 @@
+/*
+ * 
+ */
+package pkg2024_itess_tics_ti20x_ci_portafolio;
+
+/**
+ *
+ * @author jesus++
+ */
+public class Pkg2024_itess_tics_ti20x_ci_portafolio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+    }
+    
+}
